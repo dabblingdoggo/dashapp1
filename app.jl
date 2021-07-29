00f63800a317e9d04c8c5f7c68cc97b81c499cb7
dabblingdoggo <dabblingdoggo@gmail.com>
2021-07-29 17:08:32 -0600

    Initial commit

---
+++ app.jl
@@ -0,0 +1,17 @@
+using Dash, DashHtmlComponents, DashCoreComponents
+
+app = dash()
+
+app.layout = html_div() do
+    html_h1("Hello, World!")
+end
+
+# for local viewing only
+
+# run_server(app, "0.0.0.0", debug = true)
+
+# for Heroku
+
+port = parse(Int64, ENV["PORT"])
+
+run_server(app, "0.0.0.0", port)
